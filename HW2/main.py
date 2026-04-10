04628a11a8ad0c45d5febe22cb76ee644faadefd
Yaroslav Volkov <yvi_7693@icloud.com>
2026-04-10 22:43:07 +0300

    feat: hw2

--- HW2/main.py
+++ HW2/main.py
@@ -1,4 +1,5 @@
 
+
 def factorial(n: int) -> int:
     if not isinstance(n, int):  raise TypeError()
     if n < 0 or n > 20:  raise ValueError()
@@ -27,5 +28,45 @@ def fibonacci(n: int) -> list[int]:
         i += 1
 
     return results
+
+
+def count_ones(n: int) -> int:
+    if not isinstance(n, int):  raise TypeError()
+    if n < 0:  raise ValueError()
+
+    result = []
+
+    while n >= 1:
+        result.append(n % 2)
+        n //= 2
+
+    result.reverse()
+
+    str_result = ""
+
+    for item in result:
+        str_result += str(item)
+
+    return int(str_result)
+
+
+def is_palindrome(x: int) -> bool:
+    if not isinstance(x, int):  raise TypeError()
+    if x <= -2**31 or x >= 2**31 - 1:  raise ValueError()
+
+    result = 0
+    num = x
+
+    while num > 0:
+
+        result *= 10
+        result += num % 10
+        num //= 10
+
+    return result == x
+
+
+
+
 
 
